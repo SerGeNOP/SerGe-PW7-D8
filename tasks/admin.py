e496d3b15a9ef6dbd604f3b4e4e9f684716379ff
SerGeNOP <3750101@mail.ru>
2020-09-01 11:10:30 +0700

    Try to display category right to admin (try 2)

--- tasks/admin.py
+++ tasks/admin.py
@@ -3,16 +3,17 @@ from django.contrib import admin
 from tasks.models import TodoItem, Category
 
 
-def category(instance):
-    return instance.category
-
-
 @admin.register(TodoItem)
 class TodoItemAdmin(admin.ModelAdmin):
     list_display = ('description', 'category', 'is_completed', 'created')
+
+    def category(self, instance):
+        return instance.category
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('slug', 'name')
+
+
 
